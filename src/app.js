b598f869d8eaa40bf0f0b3678189064803387f7e
kamax0123 <angelfirst930124@gmail.com>
2022-03-05 19:33:49 +0300

    update the app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send("Hello world")
+  res.status(200).send("It is very pleasure me")
 })
 module.exports = app;
