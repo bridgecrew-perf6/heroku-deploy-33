5f59b9346b69a08d30da9a979a98a903ea63cd64
kamax0123 <angelfirst930124@gmail.com>
2022-03-06 20:28:28 +0300

    some changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send("It is very pleasure me")
+  res.status(200).send(process.env.key);
 })
 module.exports = app;
