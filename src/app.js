4481a3be2cbb5091ad7a5b1928fea7390c21c8c9
kamax0123 <angelfirst930124@gmail.com>
2022-03-06 23:06:48 +0300

    some changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send(process.env.KEY);
+  res.status(200).send(`${process.env.KEY}`);
 })
 module.exports = app;
